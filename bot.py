50c7469cc8e56527df4c2d02a1492567391561bc
Chetan Kumar <112582415+kumar-chetan@users.noreply.github.com>
2025-02-10 19:44:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from instabot import Bot
 bot = Bot()
-bot.login(username="i_ai_animes",password='1stJanuary@2024')
-bot.upload_video("segment_1.mp4",caption="Anime")
+bot.login(username="i_ai_animes",password='January')
+bot.upload_video("segment_1.mp4",caption="Anime reel")
 print("Finally")
